e0c6deb9d8d81b7d8477cf5178cf8532b51ec148
Danny <contact@danfoster.io>
2018-03-17 20:34:48 -0500

    Passing 22/11 tests; WIP

---
+++ [4 kyu] ipValidation.js
@@ -0,0 +1,19 @@
+/*
+IP Validation
+===
+
+Write an algorithm that will identify valid IPv4 addresses in dot-decimal format. IPs should be considered valid if they consist of four octets, with values between 0..255 (included).
+
+Input to the function is guaranteed to be a single string.
+*/
+
+function ipValidation(num) {
+  const ipArr = num.split('.');
+  if (ipArr.some(subSec => subSec[0] == 0)) {
+    return false;
+  } else if (ipArr.every(subSec => subSec > 0 && subSec <= 255)) {
+    return true;
+  }
+  return false;
+}
+console.log(ipValidation('123.456.78.90'));
