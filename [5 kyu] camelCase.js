c95cb60181eab6cf05b5d3f7849924c81c1dde7e
Danny <contact@danfoster.io>
2018-04-03 11:25:56 -0500

    WIP

--- [5 kyu] camelCase.js
+++ [5 kyu] camelCase.js
@@ -1,7 +1,8 @@
 /*
 Convert string to camel case
 ===
-Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized.
+Complete the method/function so that it converts dash/underscore delimited words into camel casing.
+The first word within the output should be capitalized only if the original word was capitalized.
 */
 /**
  * // returns "theStealthWarrior"
@@ -10,16 +11,3 @@ toCamelCase("the-stealth-warrior")
 // returns "TheStealthWarrior"
 toCamelCase("The_Stealth_Warrior")
  */
-
-const toCamelCase = str => {
-  let strArr = [];
-  if (str.includes('-')) {
-    strArr = str.split('-');
-    if (strArr[0].charAt(0) === strArr[0].charAt(0).toUpperCase()) {
-      return console.log('it works');
-    }
-  } else if (str.includes('_')) {
-  }
-};
-
-console.log(toCamelCase('The-Stealth-Warrior'));
