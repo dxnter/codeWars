0ca6b58bcf20ee83b22162fc44bcb2085d94305c
Danny <contact@danfoster.io>
2018-04-21 00:00:25 -0500

    camelCase passing

--- [5 kyu] camelCase.js
+++ [5 kyu] camelCase.js
@@ -11,3 +11,24 @@ toCamelCase("the-stealth-warrior")
 // returns "TheStealthWarrior"
 toCamelCase("The_Stealth_Warrior")
  */
+
+const toCamelCase = str => {
+  let splitSeparator = '';
+  if (str.includes('-')) splitSeparator = '-';
+  if (str.includes('_')) splitSeparator = '_';
+
+  return str
+    .split(splitSeparator)
+    .map((word, index) => {
+      if (index === 0 || word.charAt(0) === word.charAt(0).toUpperCase()) {
+        return word;
+      }
+      return word.charAt(0).toUpperCase() + word.substr(1);
+    })
+    .join('');
+};
+
+console.log(toCamelCase(''));
+console.log(toCamelCase('the_stealth_warrior'));
+console.log(toCamelCase('the-Stealth-Warrior'));
+console.log(toCamelCase('A-B-C'));
