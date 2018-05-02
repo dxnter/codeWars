b78055b1411b8b6f12555c6a580c1c79d2ae62a7
Danny <contact@danfoster.io>
2018-05-01 19:41:42 -0500

    stripComments final

---
+++ [4 kyu] stripComments.js
@@ -0,0 +1,20 @@
+/*
+Strip Comments
+===
+https://www.codewars.com/kata/strip-comments
+*/
+
+const solution = (input, markers) =>
+  input
+    .split('\n')
+    .map(sentence => {
+      const commentMarker = markers.find(marker => sentence.includes(marker));
+      if (commentMarker) {
+        console.log(commentMarker);
+        return sentence.slice(0, sentence.indexOf(commentMarker)).trim();
+      }
+      return sentence.trim();
+    })
+    .join('\n');
+
+console.log(solution('apples, pears # and bananas\ngrapes\nbananas !apples', ['#', '!']));
