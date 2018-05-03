0284dd0682a6b1f32fa17adc842d13600a71fe9b
Danny <contact@danfoster.io>
2018-05-03 01:25:58 -0500

    Final

---
+++ [5 kyu] ISBN-10.js
@@ -0,0 +1,20 @@
+/*
+ISBN-10 Validation
+===
+https://www.codewars.com/kata/isbn-10-validation/train/javascript
+*/
+
+const validISBN10 = isbn => {
+  if (isbn.length !== 10) return false;
+  const isbnArray = isbn
+    .split('')
+    .map((number, index) => {
+      if (number === 'X' && index === isbn.length - 1) return 10;
+      return number;
+    })
+    .map(Number);
+  if (isbnArray.includes(NaN)) return false;
+  return isbnArray.reduce((total, number, index) => total + number * (index + 1)) % 11 === 0;
+};
+
+console.log(validISBN10('X123456788'));
